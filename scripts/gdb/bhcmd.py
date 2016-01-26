021e2197944df21499a6ea5107056e2f47d89716
Alessandro Molari <molari.alessandro@gmail.com>
2016-01-26 17:11:03 +0000

    Bug fix

--- scripts/gdb/bhcmd.py
+++ scripts/gdb/bhcmd.py
@@ -13,33 +13,33 @@ def clean_remaining(remaining):
 class BHBp(gdb.Breakpoint):
     def __init__(self, spec, callback, location, remaining):
         super(BHBp, self).__init__(spec, gdb.BP_BREAKPOINT, internal=False)
-        self.callback = callback
-        self.location = location
-        self.remaining = remaining
+        self._callback = callback
+        self._location = location
+        self._remaining = remaining
 
     def stop(self):
-        self.callback(self.location, clean_remaining(self.remaining))
+        self._callback(self._location, clean_remaining(self._remaining))
         return False
 
 class BHCmd(gdb.Command):
 
     def __init__(self, name, callback, verbose=False):
         super(BHCmd, self).__init__(name, gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL)
-        self.callback = callback
-        self.verbose = verbose
+        self._callback = callback
+        self._verbose = verbose
 
     def invoke(self, arg, from_tty):
         (remaining, locations) = gdb.decode_line(arg)
-        if self.verbose:
+        if self._verbose:
             print("-> Argument: %s"  % (arg,))
             print("-> Remaining: %s" % (remaining,))
         if len(locations) != 1:
             gdb.GdbError("The command `BHCmd` supports just one location.")
         location = locations[0]
-        if self.verbose:
+        if self._verbose:
             print("-> Location %s" % (location,))
-        spec = "*0x%s" % (location.pc,)
-        BHBp(spec, self.callback, location, remaining)
+        spec = arg[0:-len(remaining)]
+        BHBp(spec, self._callback, location, remaining)
 
 # }}}
 
