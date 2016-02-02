c142b0042da50dc98f1e2fa3575c0be8dfc7eefb
Alessandro Molari <molari.alessandro@gmail.com>
2016-02-02 12:35:31 +0000

    Update bhcmd.py

--- scripts/gdb/bhcmd.py
+++ scripts/gdb/bhcmd.py
@@ -1,6 +1,7 @@
 # {{{ Imports ------------------------------------------------------------------
 
 import gdb
+import sys
 from re import sub
 from itertools import tee
 try:
@@ -45,24 +46,39 @@ def bp_info():
             else:
                 bp["uncategorized"].append(breakpoint_str)
         else:
-            print("Can't parse the line: %s: skipping.." % (breakpoint_str,))
+            print("Can't parse the line: {}: skipping..".format(breakpoint_str))
     return info
 
 class BHBp(gdb.Breakpoint):
     def __init__(self, spec, callback, location, remaining, go_on=False):
         super(BHBp, self).__init__(spec, gdb.BP_BREAKPOINT, internal=False)
+        self._go_on = go_on
         self._callback = callback
         self._location = location
         self._remaining = remaining
 
     def stop(self):
-        callback = lambda: self._callback(self._location, clean_remaining(self._remaining))
-        if go_on:
+        callback = lambda: self._callback(self._location,
+                                          clean_remaining(self._remaining))
+        if self._go_on:
             callback = lambda: (callback(), gdb.execute("continue"))
         gdb.post_event(callback)
         return True
 
 class BHCmd(gdb.Command):
+
+    @staticmethod
+    def create(name, callback, verbose=False, go_on=False, prefix="c"):
+        cmd_name = "{prefix}_{name}".format(prefix=prefix, name=name)
+        cmd_name_loop = "{name}_loop".format(name=cmd_name)
+
+        cmd      = BHCmd(cmd_name,      callback, verbose, go_on)
+        cmd_loop = BHCmd(cmd_name_loop, callback, verbose, go_on)
+
+        setattr(sys.modules[__name__], cmd_name,      cmd)
+        setattr(sys.modules[__name__], cmd_name_loop, cmd_loop)
+
+        return [cmd, cmd_loop]
 
     def __init__(self, name, callback, verbose=False, go_on=False):
         super(BHCmd, self).__init__(name, gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL)
@@ -73,29 +89,23 @@ class BHCmd(gdb.Command):
     def invoke(self, arg, from_tty):
         (remaining, locations) = gdb.decode_line(arg)
         if self._verbose:
-            print("-> Argument: %s"  % (arg,))
-            print("-> Remaining: %s" % (remaining,))
+            print("-> Argument: {}".format(arg))
+            print("-> Remaining: {}".format(remaining))
         if len(locations) != 1:
             gdb.GdbError("The command `BHCmd` supports just one location.")
         location = locations[0]
         if self._verbose:
-            print("-> Location %s" % (location,))
-        if remaining is not None:
-            spec = arg[0:-len(remaining)]
-        else:
-            spec = arg[:]
-        BHBp(spec, self._callback, location, remaining, go_on)
+            print("-> Location {}".format(location))
+        spec = remaining and arg[0:-len(remaining)] or arg[:]
+        BHBp(spec, self._callback, location, remaining, self._go_on)
 
 # }}} --------------------------------------------------------------------------
 # {{{ Custom commands ----------------------------------------------------------
 
-c_examine = BHCmd("c_examine", lambda _, r: gdb.execute("x/%s" % (r,)))
-c_printf  = BHCmd("c_printf",  lambda _, r: gdb.execute("printf %s" % (r,)))
-c_where   = BHCmd("c_where",   lambda _l, _r: (
-    gdb.execute("stepi"),
-    gdb.execute("where")))
-
-loop_where = LoopCmd("loop_where", c_where)
+BHCmd.create("examine", lambda _,   r:  gdb.execute("x/{}".format(r)))
+BHCmd.create("printf",  lambda _,   r:  gdb.execute("printf {}".format(r)))
+BHCmd.create("where",   lambda _l, _r: (gdb.execute("stepi"),
+                                        gdb.execute("where")))
 
 # }}}
 
